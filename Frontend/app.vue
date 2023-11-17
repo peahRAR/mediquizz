145fa5771deac651aee454c1d67dfde30ff9a76e
peahRAR <padecallonne@gmail.com>
2023-11-17 23:16:47 +0100

    start frontend section

---
+++ Frontend/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-blue-950 h-screen">
+    <header>
+      <my-header/>
+    </header>
+  </div>
+</template>
---
+++ Frontend/components/myHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container m-auto flex justify-center h-1/6">
+        <nuxt-img class="py-2" format="webp" densities="x1 x2" src="/mediquiz-web-logo.png"  />
+    </div>
+</template>
